5d70bc79572f4d27fef75d9aeb4636b689703bc6
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2023-11-14 17:52:15 +0300

    last fixes

--- Assets/Scripts/UI/ParalaxBehaviour.cs
+++ Assets/Scripts/UI/ParalaxBehaviour.cs
@@ -1,36 +1,43 @@
 using Boot;
+using Player.Movement;
 using UnityEngine;
 
 namespace Decorations
 {
     public sealed class ParalaxBehaviour : MonoBehaviour, IBoot
     {
-        [SerializeField] private Transform followingTarget;
+        [SerializeField] private PlayerMovementControl followingTarget;
+
+        private Transform playerCameraTransform;
+
         private Vector3 targetPreviousPosition;
+
         [SerializeField, Range(0f, 1f)] private float paralaxStrength = 0.1f;
+
         [SerializeField] private bool disableVerticalParallax;
+
+
+        void IBoot.InitAwake()
+        {
+            if (!followingTarget)
+                playerCameraTransform = followingTarget.GetComponentInChildren<Camera>().transform;
+
+            targetPreviousPosition = transform.position;
+        }
 
         (Bootstrap.TypeLoadObject typeLoad, Bootstrap.TypeSingleOrLotsOf singleOrLotsOf) IBoot.GetTypeLoad()
         {
             return (Bootstrap.TypeLoadObject.SimpleImportant, Bootstrap.TypeSingleOrLotsOf.Single);
         }
 
-        void IBoot.InitAwake()
-        {
-            if (!followingTarget)
-                followingTarget = Camera.main.transform;
-
-            targetPreviousPosition = transform.position;
-        }
-
         private void FixedUpdate()
         {
-            var delta = followingTarget.position - targetPreviousPosition;
+            var delta = playerCameraTransform.position - targetPreviousPosition;
 
             if (disableVerticalParallax)
                 delta.y = 0;
 
-            targetPreviousPosition = followingTarget.position;
+            targetPreviousPosition = playerCameraTransform.position;
             transform.position += delta * paralaxStrength;
         }
     }
