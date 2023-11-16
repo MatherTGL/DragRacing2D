98bfc4eaa41243d52fd4eb6429c3ac6178ad9259
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2023-11-16 20:28:08 +0300

    last worked

--- Assets/Scripts/Garage/UI/GarageView.cs
+++ Assets/Scripts/Garage/UI/GarageView.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using Boot;
 using Boot.SceneLoader;
+using Garage.PlayerCar;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Garage.UI
 {
@@ -12,6 +15,9 @@ namespace Garage.UI
         [SerializeField, Required]
         private SceneLoader _sceneLoader;
 
+        [SerializeField, Required]
+        private Image _carBodySprite;
+
         private byte _currentCarIndex;
 
 
@@ -19,6 +25,7 @@ namespace Garage.UI
         {
             _IgarageControl = FindObjectOfType<GarageControl>();
             _sceneLoader ??= FindObjectOfType<SceneLoader>();
+            _carBodySprite.sprite = PlayerSelectedCar.selectedCar.bodyImage.sprite;
         }
 
         (Bootstrap.TypeLoadObject typeLoad, Bootstrap.TypeSingleOrLotsOf singleOrLotsOf) IBoot.GetTypeLoad()
@@ -34,12 +41,10 @@ namespace Garage.UI
                 _currentCarIndex++;
 
             _IgarageControl.ChangeCar(_currentCarIndex);
+            _carBodySprite.sprite = PlayerSelectedCar.selectedCar.config.bodySprite;
         }
 
-        public void SellCar()
-        {
-            _IgarageControl.SellCar(_currentCarIndex);
-        }
+        public void SellCar() => _IgarageControl.SellCar(_currentCarIndex);
 
         public void StartRacing()
         {
