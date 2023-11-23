2c1f3fe409079c8d92577ecbb14ddd13e6762609
Joe Biden <dominserv123@mail.ru>
2023-11-23 16:59:23 +0300

    BrakeFix+FixCam

--- Assets/Scripts/UI/ParalaxBehaviour.cs
+++ Assets/Scripts/UI/ParalaxBehaviour.cs
@@ -1,45 +1,29 @@
-using Boot;
-using Player.Movement;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Decorations
+public class ParalaxBehaviour : MonoBehaviour
 {
-    public sealed class ParalaxBehaviour : MonoBehaviour, IBoot
+    [SerializeField] Transform followingTarget;
+    [SerializeField, Range (0f,1f)] float paralaxStrength = 0.1f;
+    [SerializeField] bool disableVerticalParallax;
+    Vector3 targetPreviousPosition;
+    // Start is called before the first frame update
+    void Start()
     {
-        private PlayerMovementControl followingTarget;
+        if (!followingTarget)
+            followingTarget = Camera.main.transform;
+        targetPreviousPosition = transform.position;
+    }
 
-        private Transform playerCameraTransform;
-
-        private Vector3 targetPreviousPosition;
-
-        [SerializeField, Range(0f, 1f)] private float paralaxStrength = 0.1f;
-
-        private bool disableVerticalParallax = true;
-
-
-        void IBoot.InitAwake()
-        {
-            if (!followingTarget)
-                followingTarget = FindObjectOfType<PlayerMovementControl>();
-
-            playerCameraTransform = followingTarget.GetComponentInChildren<Camera>().transform;
-            targetPreviousPosition = transform.position;
-        }
-
-        (Bootstrap.TypeLoadObject typeLoad, Bootstrap.TypeSingleOrLotsOf singleOrLotsOf) IBoot.GetTypeLoad()
-        {
-            return (Bootstrap.TypeLoadObject.SimpleImportant, Bootstrap.TypeSingleOrLotsOf.LotsOf);
-        }
-
-        private void FixedUpdate()
-        {
-            var delta = playerCameraTransform.position - targetPreviousPosition;
-
-            if (disableVerticalParallax)
-                delta.y = 0;
-
-            targetPreviousPosition = playerCameraTransform.position;
-            transform.position += delta * paralaxStrength;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        var delta = followingTarget.position - targetPreviousPosition;
+        
+        if (disableVerticalParallax)
+            delta.y = 0;
+        targetPreviousPosition = followingTarget.position;
+        transform.position += delta * paralaxStrength;
     }
 }
