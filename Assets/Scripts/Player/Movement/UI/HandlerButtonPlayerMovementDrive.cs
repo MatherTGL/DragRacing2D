2904b529653dc4a41c067f08a02617cacb2140bf
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2023-11-24 21:27:55 +0300

    Update HandlerButtonPlayerMovementDrive.cs

--- Assets/Scripts/Player/Movement/UI/HandlerButtonPlayerMovementDrive.cs
+++ Assets/Scripts/Player/Movement/UI/HandlerButtonPlayerMovementDrive.cs
@@ -12,13 +12,34 @@ namespace UI.Player.Movement
         private TypeButtonMovement _typeButtonMovement;
         TypeButtonMovement IHandlerButtonPlayerMovement.typeButtonMovement => _typeButtonMovement;
 
+        [SerializeField, Required]
+        private AudioSource _raceAudio;
+
+        [SerializeField, Required]
+        private AudioSource _brakeAudio;
+
         private bool _isPressed;
         bool IHandlerButtonPlayerMovement.isPressed => _isPressed;
 
 
-        private void OnMouseDown() => _isPressed = true;
+        private void OnMouseDown()
+        {
+            if (_typeButtonMovement is TypeButtonMovement.Drive)
+                _raceAudio.Play();
+            else
+                _brakeAudio.Play();
+            _isPressed = true;
+        }
 
-        private void OnMouseUp() => _isPressed = false;
+        private void OnMouseUp()
+        {
+            if (_typeButtonMovement is TypeButtonMovement.Drive)
+                _raceAudio.Stop();
+            else
+                _brakeAudio.Stop();
+
+            _isPressed = false;
+        }
     }
 }
 
