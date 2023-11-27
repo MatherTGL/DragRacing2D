be59d8870581825b4db46fae16dfe65c04ef8ab9
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2023-11-27 15:17:41 +0300

    Update MenuView.cs

--- Assets/Scripts/UI/Menu/MenuView.cs
+++ Assets/Scripts/UI/Menu/MenuView.cs
@@ -64,7 +64,6 @@ public sealed class MenuView : MonoBehaviour, IBoot
     public void StartNewGame()
     {
         PlayerPrefs.DeleteAll();
-        PlayerPrefs.DeleteKey("Money");
         StartAudioClickButton();
         SceneManager.LoadScene(2);
     }
